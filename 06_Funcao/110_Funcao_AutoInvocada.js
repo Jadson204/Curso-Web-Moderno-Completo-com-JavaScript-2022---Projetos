feb2924c43a2328ae36335cb1a4ff6d5f2d002af
Jadson204 <jadsonhenrique2004@gmail.com>
2023-04-28 10:51:10 -0300

    110. IIFE

---
+++ 06_Funcao/110_Funcao_AutoInvocada.js
@@ -0,0 +1,5 @@
+// IIFE -> Immediately Invoked Function Expression (Expressão de Função Invocada Imediatamente)
+(function() {
+  console.log('Será executado na hora');
+  console.log('Foge do escopo global !!!')
+})();
